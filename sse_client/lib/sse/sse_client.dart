43591f08403f32f85e1877bbc3c0dfaa3a545b84
SantoshNiroula <niroulasantosh624@gmail.com>
2025-01-22 14:35:13 +0545

    add: add exponential backoff for retry

--- sse_client/lib/sse/sse_client.dart
+++ sse_client/lib/sse/sse_client.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'dart:developer';
 import 'dart:io';
+import 'dart:math' as math;
 
 import 'package:http/http.dart' as http;
 import 'package:sse_client/sse/sse_model.dart';
@@ -8,6 +9,7 @@ import 'package:sse_client/sse/sse_model.dart';
 class SseClient {
   final http.Client _client;
   final StreamController<SseModel> _controller;
+  int _retry = 0;
 
   SseClient()
       : _client = http.Client(),
@@ -17,6 +19,10 @@ class SseClient {
 
   Future<void> start() async {
     try {
+      await _backOffPolicy();
+
+      print("=== Requesting ====");
+
       final url = Platform.isAndroid ? '10.2.2.1' : 'http://127.0.0.1';
       final request = http.Request('GET', Uri.parse('$url:8080/events'));
 
@@ -29,6 +35,8 @@ class SseClient {
       });
     } on http.ClientException catch (e) {
       log(e.toString());
+      _retry++;
+      start();
     } catch (e) {
       log("Error: ${e.toString()}");
     }
@@ -37,5 +45,13 @@ class SseClient {
   void disponse() {
     _client.close();
     _controller.close();
+  }
+
+  Future<void> _backOffPolicy() async {
+    final time = math.pow(2, _retry);
+
+    if (time > 60 * 60) throw Exception('MAX retry time out');
+
+    await Future.delayed(Duration(seconds: time as int));
   }
 }
